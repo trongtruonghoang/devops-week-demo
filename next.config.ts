998da0f4e36a08c36abb03d845fbd6d97882f02a
Trong Truong <trongtruong2509@gmail.com>
2025-03-07 16:51:07 +0700

    update page to devops week 2025

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  reactStrictMode: true,
+  webpack(config) {
+    config.watchOptions = {
+      poll: 1000, // Check for changes every second
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
   /* config options here */
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,67 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
+      <header className="flex items-center justify-between px-2 w-full">
+      <Image
+          className="dark:invert p-4"
           src="/endava.svg"
           alt="Endava logo"
-          width={360}
-          height={76}
+          width={180}
+          height={38}
           priority
         />
+        <div className="flex items-center gap-2 justify-between font-[family-name:var(--font-geist-mono)] text-xl">
+          <p>
+            AM & Infra Discipline
+          </p>
+          <div className="text-2xl text-[#FF5733]">&bull;</div>
+          <p>
+            DevOps Community
+          </p>
+        </div>
+      </header>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="flex flex-col items-center gap-3 justify-between font-[family-name:var(--font-geist-mono)]">
+          <div className="flex items-center gap-1 justify-between text-6xl font-[family-name:var(--font-geist-mono)] font-bold">
+            <div className="text-[#FF5733]">&lt;</div>
+            <div>DevOps Week 2025</div>
+            <div className="text-[#FF5733]">&gt;</div>
+          </div>
+          <div className="flex gap-2">
+            <p className="text-2xl">
+              Share<span className="text-[#FF5733]">.</span>
+            </p>
+            <p className="text-2xl">
+              Learn<span className="text-[#FF5733]">.</span>
+            </p>
+            <p className="text-2xl">
+              Grow<span className="text-[#FF5733]">.</span>
+            </p>
+            
+          </div>
+        </div>
+        
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            {" "}
+            {/* {" "} */}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-            We build it - We run it
+            Date: 
             </code>
-            .
+            {" "} March 10-14 (Mon - Fri)
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+              Location:
+            </code>
+            {" "} Floor G. Training Room - Online</li>
+          <li>
+            {/* <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+              Location:
+            </code> */}
+            For Anyone ready to dive into DevOps</li>
         </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://www.endava.com/"
@@ -47,7 +87,7 @@ export default function Home() {
           >
             Read our docs
           </a>
-        </div>
+        </div> */}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
